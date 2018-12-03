b3485cc0aece5c0435ce865124281a6e809a2051
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-12-03 17:06:02 +0100

    Scroll to top on navigation change

--- app/assets/javascripts/components/buttons/generic-button.jsx
+++ app/assets/javascripts/components/buttons/generic-button.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 export default class GenericButton extends React.Component {
+  static defaultProps = {
+    label: "Okay",
+    disabled: false,
+    className: "",
+    major: false,
+    onClick: null,
+    href: null
+  };
+
   render() {
     const classes = this.props.className.split(/\s+/);
     classes.push(this.props.major ? "major-button" : "minor-button");
@@ -10,34 +20,36 @@ export default class GenericButton extends React.Component {
 
     let { onClick } = this.props;
 
-    if (this.props.href) {
-      const c = this.props.onClick;
-      onClick = () => {
-        if (typeof c === "function") {
-          c();
-        }
-        window.location.href = this.props.href;
-      };
+    if (this.props.to) {
+      return (
+        <NavLink className={classes.join(" ")}
+          to={this.props.to}
+          disabled={this.props.disabled ? "disabled" : undefined}>
+          <span>{this.props.label}</span>
+        </NavLink>
+      );
     }
+    else {
+      if (this.props.href) {
+        const c = this.props.onClick;
+        onClick = () => {
+          if (typeof c === "function") {
+            c();
+          }
+          window.location.href = this.props.href;
+        };
+      }
 
-    const key = this.props.href || this.props.onClick;
+      const key = this.props.href || this.props.onClick;
 
-    return (
-      <button key={key}
-        className={classes.join(" ")}
-        onClick={onClick}
-        disabled={this.props.disabled ? "disabled" : undefined}>
-        <span>{this.props.label}</span>
-      </button>
-    );
+      return (
+        <button key={key}
+          className={classes.join(" ")}
+          onClick={onClick}
+          disabled={this.props.disabled ? "disabled" : undefined}>
+          <span>{this.props.label}</span>
+        </button>
+      );
+    }
   }
-};
-
-GenericButton.defaultProps = {
-  label: "Okay",
-  disabled: false,
-  className: "",
-  major: false,
-  onClick: null,
-  href: null
 };
