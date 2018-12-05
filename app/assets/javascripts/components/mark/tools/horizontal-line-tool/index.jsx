79e0ab26446d6594afbc49fd833d20336692cc7e
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-12-05 15:04:19 +0100

    Linting compatibility newer React

--- app/assets/javascripts/components/mark/tools/horizontal-line-tool/index.jsx
+++ app/assets/javascripts/components/mark/tools/horizontal-line-tool/index.jsx
@@ -1,84 +1,82 @@
-import React from 'react';
-import createReactClass from "create-react-class";
-import PropTypes from 'prop-types';
-import Draggable from "../../../../lib/draggable.jsx";
-import DeleteButton from "../../../../components/buttons/delete-mark.jsx";
-import MarkButtonMixin from "../../../../lib/mark-button-mixin.jsx";
-import MarkLabel from '../mark-label.jsx';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Draggable from '../../../../lib/draggable.jsx'
+import DeleteButton from '../../../../components/buttons/delete-mark.jsx'
+import MarkButtonMixin from '../../../../lib/mark-button-mixin.jsx'
+import MarkLabel from '../mark-label.jsx'
 
 const STROKE_WIDTH = 10
 const DEFAULT_WIDTH = 1000
 const DELETE_BUTTON_DISTANCE_X = 12
 const DELETE_BUTTON_DISTANCE_Y = 0
 
-export default createReactClass({
-  displayName: 'HorizontalLineTool',
-
-  mixins: [MarkButtonMixin],
-
-  propTypes: {
+@MarkButtonMixin
+export default class HorizontalLineTool extends React.Component {
+  static propTypes = {
     // key:  PropTypes.number.isRequired
     mark: PropTypes.object.isRequired
-  },
-  initCoords: null,
+  }
+  initCoords = null;
 
-  statics: {
-    defaultValues: ({ x, y }) => ({
-      x: 0,
-      y: y,
-      width: DEFAULT_WIDTH,
-      height: 0
-    }),
+  static defaultValues = ({ y }) => ({
+    x: 0,
+    y: y,
+    width: DEFAULT_WIDTH,
+    height: 0
+  })
 
-    initStart: ({ x, y }, mark) => {
+  static initStart({ x, y }, mark) {
+    x = 0
+    this.initCoords = { x, y }
+    return { x, y }
+  }
+
+  static initMove(cursor, mark) {
+    let width, height, x, y
+    if (cursor.x > this.initCoords.x) {
+      width = cursor.x - mark.x
       x = 0
-      this.initCoords = { x, y }
-      return { x, y }
-    },
+    } else {
+      width = this.initCoords.x - cursor.x
+      x = 0
+    }
+    if (cursor.y > this.initCoords.y) {
+      height = cursor.y - mark.y
+      y = mark.y
+    } else {
+      height = this.initCoords.y - cursor.y
+      y = cursor.y
+    }
 
-    initMove: (cursor, mark) => {
-      let width, height, x, y
-      if (cursor.x > this.initCoords.x) {
-        width = cursor.x - mark.x
-        x = 0
-      } else {
-        width = this.initCoords.x - cursor.x
-        x = 0
-      }
-      if (cursor.y > this.initCoords.y) {
-        height = cursor.y - mark.y
-        y = mark.y
-      } else {
-        height = this.initCoords.y - cursor.y
-        y = cursor.y
-      }
+    return { x, y, width, height }
+  }
 
-      return { x, y, width, height }
-    },
-    initValid: (mark) =>
-      true,
+  static initValid(mark) {
+    return true
+  }
 
-    /**
-     * This callback is called on mouseup to override mark properties (e.g. if too small)
-     */
-    initRelease: (coords, mark, e) => {
-      mark.width = Math.max(mark.width, DEFAULT_WIDTH)
-      mark.height = STROKE_WIDTH
-      return mark
-    },
-  },
-  getInitialState() {
+  /**
+   * This callback is called on mouseup to override mark properties (e.g. if too small)
+   */
+  static initRelease(coords, mark, e) {
+    mark.width = Math.max(mark.width, DEFAULT_WIDTH)
+    mark.height = STROKE_WIDTH
+    return mark
+  }
+
+  constructor(props) {
+    super(props)
     let mark = this.props.mark
     if (mark.status == null) {
       mark.status = 'mark'
     }
 
-    return {
+    this.state = {
       mark: mark,
       buttonDisabled: false,
       lockTool: false
     }
-  },
+  }
 
   handleMainDrag(e, d) {
     if (this.state.locked) {
@@ -90,8 +88,8 @@ export default createReactClass({
     this.props.mark.x += d.x / this.props.xScale
     this.props.mark.y += d.y / this.props.yScale
     this.assertBounds()
-    return this.props.onChange(e);
-  },
+    return this.props.onChange(e)
+  }
 
   assertBounds() {
     this.props.mark.x = 0
@@ -101,31 +99,37 @@ export default createReactClass({
 
     this.props.mark.width = this.props.sizeRect.attributes.width.value
     this.props.mark.height = STROKE_WIDTH
-  },
+  }
+
   validVert(y, h) {
     return y >= 0 && y + h <= this.props.sizeRect.attributes.height.value
-  },
+  }
+
   validHoriz(x, w) {
     return x >= 0 && x + w <= this.props.sizeRect.attributes.width.value
-  },
+  }
+
   getDeleteButtonPosition() {
     let points = [this.props.mark.x + this.props.mark.width, this.props.mark.y],
       x = points[0] + DELETE_BUTTON_DISTANCE_X / this.props.xScale,
       y = points[1] + DELETE_BUTTON_DISTANCE_Y / this.props.yScale
-    x = Math.min(x, this.props.sizeRect.attributes.width.value - 15 / this.props.xScale),
-      y = Math.max(y, 15 / this.props.yScale)
+    x = Math.min(x, this.props.sizeRect.attributes.width.value - 15 / this.props.xScale)
+    y = Math.max(y, 15 / this.props.yScale)
     return { x, y }
-  },
+  }
+
   getMarkButtonPosition() {
     let points = [this.props.mark.x + this.props.mark.width, this.props.mark.y + this.props.mark.height]
     return {
       x: Math.min(points[0], this.props.sizeRect.attributes.width.value - 40 / this.props.xScale),
       y: Math.min(points[1] + 20 / this.props.yScale, this.props.sizeRect.attributes.height.value - 15 / this.props.yScale)
     }
-  },
+  }
+
   handleMouseDown() {
     this.props.onSelect(this.props.mark)
-  },
+  }
+
   normalizeMark() {
     if (this.props.mark.width < 0) {
       this.props.mark.x += this.props.mark.width
@@ -136,7 +140,7 @@ export default createReactClass({
       this.props.mark.height *= -1
     }
     return this.props.onChange()
-  },
+  }
   render() {
     let classes = []
     if (this.props.isTranscribable) {
@@ -147,7 +151,7 @@ export default createReactClass({
     }
     classes.push(this.props.disabled ? 'committed' : 'uncommitted')
     if (this.checkLocation()) {
-      classes.push("transcribing")
+      classes.push('transcribing')
     }
 
     let x1 = this.props.mark.x,
@@ -181,34 +185,34 @@ export default createReactClass({
           className={`horizontal-line-tool${this.props.disabled ? ' locked' : ''}`}
         >
 
-          <Draggable onDrag={this.handleMainDrag} >
+          <Draggable onDrag={this.handleMainDrag.bind(this)} >
             <g
               className={`tool-shape ${classes.join(' ')}`}
               key={points}
               dangerouslySetInnerHTML={{
                 __html: `
-                <filter id=\"dropShadow\">
-                  <feGaussianBlur in=\"SourceAlpha\" stdDeviation=\"3\" />
-                  <feOffset dx=\"2\" dy=\"4\" />
+                <filter id="dropShadow">
+                  <feGaussianBlur in="SourceAlpha" stdDeviation="3" />
+                  <feOffset dx="2" dy="4" />
                   <feMerge>
                     <feMergeNode />
-                    <feMergeNode in=\"SourceGraphic\" />
+                    <feMergeNode in="SourceGraphic" />
                   </feMerge>
                 </filter>
 
                 <rect
-                  fill=\"${this.props.mark.color || "gray"}\"
-                  x=\"${x}\"
-                  y=\"${y}\"
-                  width=\"${width}\"
-                  height=\"${height}\"
-                  filter=\"${this.props.selected ? 'url(#dropShadow)' : 'none'}\"
+                  fill="${this.props.mark.color || 'gray'}"
+                  x="${x}"
+                  y="${y}"
+                  width="${width}"
+                  height="${height}"
+                  filter="${this.props.selected ? 'url(#dropShadow)' : 'none'}"
                 />
               `
               }}
             />
           </Draggable>
-          <MarkLabel x={x1 + width / 2} y={y1 + height / 2} label={this.props.mark.label} />
+          <MarkLabel fill={this.props.mark.color} x={x1 + width / 2} y={y1 + height * (y1 < 50 ? 6 : -2)} label={this.props.mark.label} />
 
           {this.props.selected &&
             (deleteButtonPos = this.getDeleteButtonPosition()) &&
@@ -225,4 +229,4 @@ export default createReactClass({
       </g>
     )
   }
-});
+}
