2843913d768c90fb6b9fcbc5744d38b2c42125b7
Alex Hebing <a.h.j.hebing@uu.nl>
2019-02-21 10:14:18 +0100

    Completed refactor of SubjectSetSelector

--- app/assets/javascripts/components/subject-set-selector.jsx
+++ app/assets/javascripts/components/subject-set-selector.jsx
@@ -6,25 +6,23 @@ import DraggableModal from './draggable-modal.jsx'
 import GenericButton from './buttons/generic-button'
 
 export default class SubjectSetSelector extends React.Component {
-  getDefaultProps() {
-    return {
-      classes: '',
-      doneButtonLabel: 'Give me a random source'
-    }
+  static defaultProps = {
+    classes: '',
+    doneButtonLabel: 'Give me a random source'
   }
 
   sortByGroupId(subjectSets) {
-    subjectSets.sort(function(a, b) {
-      if(a.group_id < b.group_id) { return -1; }
-      if(a.group_id > b.group_id) { return 1; }
-      return 0;      
+    subjectSets.sort(function (a, b) {
+      if (a.group_id < b.group_id) { return -1; }
+      if (a.group_id > b.group_id) { return 1; }
+      return 0;
     })
   }
 
   getSubjectSets(subjectSets) {
     let subjectSetTitles = [];
     let currentLanguage;
-    
+
     if (subjectSets.length > 1) {
       this.sortByGroupId(subjectSets);
     }
@@ -34,22 +32,22 @@ export default class SubjectSetSelector extends React.Component {
       let subjectSetLan = subjectSets[i].meta_data.langs;
 
       // if Mark is navigated to via menu (not a specific language)...
-      if (!this.props.group_id) { 
+      if (!this.props.group_id) {
         // ... add language headers 
         if (i === 0 || currentLanguage !== subjectSetLan) {
           subjectSetTitles.push(<h6 key={i}>{subjectSetLan}</h6>)
           currentLanguage = subjectSetLan;
         }
       }
-      
+
       subjectSetTitles.push(
-        <GenericButton 
-            key={subjectSets[i].id}
-            label={this.parseTitle(subjectSets[i].meta_data.set_key)}
-            className="ghost small-button selectable-subject-set"
-            onClick={() => { 
-                this.onSubjectSetSelected(subjectSetId)
-            }}>                    
+        <GenericButton
+          key={subjectSets[i].id}
+          label={this.parseTitle(subjectSets[i].meta_data.set_key)}
+          className="ghost small-button selectable-subject-set"
+          onClick={() => {
+            this.onSubjectSetSelected(subjectSetId)
+          }}>
         </GenericButton>);
     }
     return <div>{subjectSetTitles}</div>;
@@ -57,7 +55,7 @@ export default class SubjectSetSelector extends React.Component {
 
   parseTitle(subjectSetKey) {
     if (subjectSetKey.indexOf('_') != -1) {
-      let splitSetKey = subjectSetKey.split('_');      
+      let splitSetKey = subjectSetKey.split('_');
       let author = splitSetKey[0].split('-').join(' ');
       let title = splitSetKey[1].split('-').join(' ');
       return author + '. ' + title + '.';
@@ -67,30 +65,30 @@ export default class SubjectSetSelector extends React.Component {
   }
 
   onSelectRandomSubjectSet() {
-      this.onSubjectSetSelected(undefined)
+    this.onSubjectSetSelected(undefined)
   }
 
-  onSubjectSetSelected(subjectSetId){
+  onSubjectSetSelected(subjectSetId) {
     let { onSelected } = this.props
     onSelected(subjectSetId);
   }
 
   render() {
     return (
-        <DraggableModal
-            ref="tutorialModal"
-            header={'Please select a source'}
-            doneButtonLabel="Give me a random source"
-            onDone={this.onSelectRandomSubjectSet}
-            width={800}
-            classes="help-modal"                    
-            closeButton={true}
-            onClose={this.onSelectRandomSubjectSet}>
-            <div>
-                <p>Please select the source you would like to work on.</p>
-                {this.getSubjectSets(this.props.subjectSets)}
-            </div>
-        </DraggableModal>
+      <DraggableModal
+        ref="tutorialModal"
+        header={'Please select a source'}
+        doneButtonLabel="Give me a random source"
+        onDone={this.onSelectRandomSubjectSet}
+        width={800}
+        classes="help-modal"
+        closeButton={true}
+        onClose={this.onSelectRandomSubjectSet}>
+        <div>
+          <p>Please select the source you would like to work on.</p>
+          {this.getSubjectSets(this.props.subjectSets)}
+        </div>
+      </DraggableModal>
     )
   }
 }
