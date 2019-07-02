422c256b4a471fb25ed5f9089178c8347ff942f5
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-07-02 21:42:24 +0200

    Set browser language as default language

--- project/skillnet/assets/js/custom.js
+++ project/skillnet/assets/js/custom.js
@@ -7,9 +7,21 @@ $(document).ready(function () {
     document.cookie = `language=${language}`
     return $(`<style>.i18n .${language} { display: inherit; }</style>`)
   }
+  function determineDefault() {
+    var languages = navigator.languages || []
+    for (var i in languages) {
+      switch (languages[i].replace(/-.*$/, '')) {
+        case 'nl':
+          return 'language=nederlands'
+        case 'en':
+          return 'language=english'
+      }
+    }
+    return 'language=english'
+  }
   var currentLanguage = document.cookie.split(';').filter(function (item) {
     return item.trim().startsWith('language=')
-  })[0] || 'language=english'
+  })[0] || determineDefault()
 
   var $langStyle = showLanguage(currentLanguage.split('=')[1])
   $(document.body).append($langStyle)
