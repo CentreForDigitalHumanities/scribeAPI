99fa08c5074d228d06b42dd61bca46bcaa5dd22a
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-03-20 09:36:19 +0100

    Consecutive subject order numbers

--- project/skillnet/script/assign_page_numbers.py
+++ project/skillnet/script/assign_page_numbers.py
@@ -9,6 +9,9 @@ args = parser.parse_args()
 
 headers = []
 rows = []
+
+set_orders = {}
+
 with open(args.file, encoding="utf-8-sig") as file:
     csv_reader = csv.DictReader(file, delimiter=',')
     headers = csv_reader.fieldnames
@@ -16,8 +19,27 @@ with open(args.file, encoding="utf-8-sig") as file:
         headers = ["order"] + headers
 
     for row in csv_reader:
-        row["order"] = int(re.search("(\d+)\.jpg$", row["file_path"]).groups(1)[0])
+        order = int(re.search("(\d+)\.jpg$", row["file_path"]).groups(1)[0])
+        row["order"] = order
+        set_key = row["set_key"]
         rows.append([row[col] for col in headers])
+
+        # need to number consecutively because pagination works
+        # based on the subject count
+        if not set_key in set_orders:
+            set_orders[set_key] = []
+        set_orders[set_key].append(order)
+
+for set in set_orders.values():
+    set.sort()
+
+set_key_index = headers.index("set_key")
+order_index = headers.index("order")
+
+for row in rows:
+    set_key = row[set_key_index]
+    order = row[order_index]
+    row[order_index] = set_orders[set_key].index(order)
 
 
 with open(args.file, 'w+', encoding="utf-8") as file:
