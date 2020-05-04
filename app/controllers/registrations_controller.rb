de37606fe950823df3988c30d3a5cc3ebd773c12
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-05-04 15:05:27 +0200

    Upgrading to Rails 4.1+ broke user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -40,8 +40,8 @@ class RegistrationsController < Devise::RegistrationsController
   end
   
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
-    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password])
   end
 
 end
