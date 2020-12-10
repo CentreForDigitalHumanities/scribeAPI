876d375a82fdc61e13282f484f0d92a85ed5e0df
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-12-10 10:44:29 +0100

    Explain :layout => false

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  # The view needs to be rerendered to update the CSRF meta tag
   caches_action :index, :layout => false, :cache_path => "home/index"
 
   def index
